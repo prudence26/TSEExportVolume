fe44f25d75d4e69043d1de7168e7e13c0b96463e
Aaron Jay Alviz <aaronjayalviz@ymail.com>
2018-05-18 11:51:03 +0800

    exportvolume

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExportVolumeComponent } from './export-volume/export-volume.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/export-volume', pathMatch: 'full'},
+  { path: 'export-volume', component: ExportVolumeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ExportVolumeComponent } from './export-volume/export-volume.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from '@progress/kendo-angular-buttons';
+import { ButtonGroupModule } from '@progress/kendo-angular-buttons';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { LayoutModule } from '@progress/kendo-angular-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExportVolumeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    ButtonGroupModule,
+    DropDownsModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
